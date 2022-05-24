506be72b1494c125fe66cf77bf3fb2075bbfe872
ThitipornPhueng <thitiporn16pts@gmail.com>
2022-05-24 14:14:55 +0700

    Update 1.hello.java

--- 1.hello.java
+++ 1.hello.java
@@ -1,5 +1,8 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World 5555!"); 
+        System.out.println(x:"NAME : Mr.THITIPORN"); 
+        System.out.println(x:"NICNAME : PHUENG");
+        System.out.println(x:"DEPART : ICT");
+        System.out.println(x:"GRADE : 4"); 
     }
 }
